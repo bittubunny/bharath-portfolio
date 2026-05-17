bd351da005e2a7c0c055b5833b159740bee679a9
Bharath Chelimalla <44095607+bittubunny@users.noreply.github.com>
2026-05-17 20:34:43 +0530

    Update logout URL in AdminDashboard

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -11,7 +11,7 @@ function AdminDashboard() {
     try {
 
       await axios.post(
-        "http://127.0.0.1:5000/logout"
+        "https://bharath-portfolio-7gje.onrender.com/logout"
       );
 
       localStorage.removeItem("isAdmin");
